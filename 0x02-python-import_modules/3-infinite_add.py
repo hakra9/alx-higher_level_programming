efe8f412881da2ed9a9bb04d5e62b0083c22e355
Your Name <you@example.com>
2023-09-07 14:21:23 -0700

    h

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     """Print the addition of all arguments."""
     import sys
 
-sum = 0
-for i in range(len(sys.argv)-1)
-    sum += int(sys.argv[i+1])
-print("{}".format(sum))
+    sum = 0
+    for i in range(len(sys.argv)-1)
+        sum += int(sys.argv[i+1])
+    print("{}".format(sum))
