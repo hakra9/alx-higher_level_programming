0fb5b6d12dba885a6720c45377bce6455ba53a64
Your Name <you@example.com>
2023-09-07 10:24:13 -0700

    h

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     """Print the sum of 1 and 2."""
-import  add_0.py
-res= add_0.add(1,2)
+from  add_0.py import add
+a=1
+b=2
+res= add(a,b)
 print (res)
