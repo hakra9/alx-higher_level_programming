20e00ce261bca6bda23698f327552a0d4eacdd43
Your Name <you@example.com>
2023-09-07 14:18:41 -0700

    h

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,7 +5,6 @@ if __name__ == "__main__":
     import sys
 
 sum = 0
-for i in range (len (sys.argv)-1)
-    sum = sum + int(sys.argv[i+1])
-    print("{}".format(sum))
-
+for i in range(len(sys.argv)-1)
+    sum += int(sys.argv[i+1])
+print("{}".format(sum))
