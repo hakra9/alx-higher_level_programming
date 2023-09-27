a441dc9d0976d83d490ec1d7f4f510e9205b9af0
Your Name <you@example.com>
2023-09-27 10:54:22 -0700

    h

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,5 +8,5 @@ def safe_print_list_integers(my_list=[], x=0):
             nb_print += 1
         except (ValueError, TypeError):
             continue
-        print("")
+    print("")
     return (nb_print)
