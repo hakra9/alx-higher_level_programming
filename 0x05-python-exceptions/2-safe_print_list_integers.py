2a470004a199142268d79c1e1090b862c54d4473
Your Name <you@example.com>
2023-09-27 10:53:42 -0700

    h

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    nb_print=0
+    nb_print = 0
     for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
@@ -9,4 +9,4 @@ def safe_print_list_integers(my_list=[], x=0):
         except (ValueError, TypeError):
             continue
         print("")
-    return (ret)
+    return (nb_print)
