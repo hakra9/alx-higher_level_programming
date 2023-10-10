a652e471fc08a77e274c232ec52153612ff01698
Your Name <you@example.com>
2023-10-10 11:12:58 -0700

    h

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,6 +2,7 @@
 '''Module for Rectangle class.'''
 Rectangle = __import__('9-rectangle').Rectangle
 
+
 class Square(Rectangle):
     '''A subclass representing a rectangle.'''
     def __init__(self, size):
