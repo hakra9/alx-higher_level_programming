e58228952c828a0ee551bf3aa0039028c4a128c9
Your Name <you@example.com>
2023-11-14 12:40:22 -0800

    sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,6 @@
---a script that lists all records of the table second_table.
+-- a script that lists all records of the table second_table.
+-- Records are ordered by descending score.
+
 SELECT `score`, `name`
 FROM `second_table`
 ORDER BY `score` DESC;
